da7baf21d16c8d7cb945b8f1aa37d2fceb545d50
pbrudny <pbrudny@gmail.com>
2017-10-15 14:58:44 +0200

    Add form

--- express_5_4/server.js
+++ express_5_4/server.js
@@ -1,10 +1,21 @@
 var express = require('express');
-
 var app = express();
 
+app.use(express.static('assets'));
+
 app.get('/', function(req, res) {
-    res.send('Hello World');
+    res.sendFile('/index.html');
 });
+
+app.get('/userform', function(req, res) {
+    console.log(req.query.first_name);
+    const response = {
+        first_name: req.query.first_name,
+        last_name: req.query.last_name
+    };
+    res.end(JSON.stringify(response));
+});
+
 
 var server = app.listen(3000, 'localhost', function() {
     var host = server.address().address;
