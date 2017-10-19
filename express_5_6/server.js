4e047447b7639da0d68c43798a4fc7c3f75f27c3
pbrudny <pbrudny@gmail.com>
2017-10-19 15:31:28 +0200

    Add auth structure

--- express_5_6/server.js
+++ express_5_6/server.js
@@ -4,33 +4,22 @@ var app = express();
 app.set('view engine', 'pug');
 app.set('views', './views');
 
-app.use('/store', function(req, res, next) {
-    console.log('Jestem pośrednikiem przy /store');
-    next();
-});
-
-app.get('/first-view', function(req, res) {
-    res.render('first-view');
-});
-
-app.get('/dynamic-view', function(req,res) {
-    res.render('dynamic', {
-        name: 'Strona dynamiczna',
-        url: 'http://ox.pl'
-    });
-});
-
-app.get('/store', function(req, res) {
-    res.send('To jest sklep');
-});
-
 app.get('/', function(req, res) {
-    res.send('Hello World');
+    res.render('index');
+});
+
+app.get('/auth/google', function(req, res) {
+    res.render('google_form');
+});
+
+app.post('/sign_in', function(req, res) {
+    res.render('welcome');
 });
 
 var server = app.listen(3000, function() {
     console.log('Słuchamy na http://localhost:3000');
 });
+
 app.use(function(req, res, next) {
     res.status(404).send('Nie ma takiej strony :(');
 });
