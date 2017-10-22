b88ed631611784e54c71604e7c9e70f9260f6321
pbrudny <pbrudny@gmail.com>
2017-10-22 15:24:51 +0200

    Add passport

---
+++ express_5_6/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    'GOOGLE_CLIENT_ID': '716130424243-ts3mmmc4kusdhvt8eh23ffrs9dduki78.apps.googleusercontent.com',
+    'GOOGLE_CLIENT_SECRET': 'kq1iPhwE3b9GrQNUlFn8Z2ye',
+    'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+}
--- express_5_6/server.js
+++ express_5_6/server.js
@@ -1,25 +1,60 @@
 var express = require('express');
+var passport = require('passport');
+var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+var config = require('./config');
 var app = express();
+var googleProfile = {};
+
+passport.serializeUser(function(user, done) {
+    done(null, user);
+});
+
+passport.deserializeUser(function(obj, done) {
+    done(null, obj);
+});
+
+passport.use(new GoogleStrategy({
+        clientID: config.GOOGLE_CLIENT_ID,
+        clientSecret: config.GOOGLE_CLIENT_SECRET,
+        callbackURL: config.CALLBACK_URL
+    },
+    function(accessToken, refreshToken, profile, cb) {
+        googleProfile = {
+            id: profile.id,
+            displayName: profile.displayName
+        };
+        cb(null, profile);
+    }
+));
 
 app.set('view engine', 'pug');
 app.set('views', './views');
+app.use(passport.initialize());
+app.use(passport.session());
 
-app.get('/', function(req, res) {
-    res.render('index');
+//app routes
+app.get('/', function(req, res){
+    res.render('index', { user: req.user });
 });
 
-app.get('/auth/google', function(req, res) {
-    res.render('google_form');
+app.get('/logged', function(req, res){
+    res.render('logged', { user: googleProfile });
 });
 
-app.post('/sign_in', function(req, res) {
-    res.render('welcome');
+app.get('/error', function(req, res){
+    res.render('error');
 });
 
-var server = app.listen(3000, function() {
-    console.log('Słuchamy na http://localhost:3000');
-});
+//Passport routes
+app.get('/auth/google',
+    passport.authenticate('google', {
+        scope : ['profile', 'email']
+    }));
 
-app.use(function(req, res, next) {
-    res.status(404).send('Nie ma takiej strony :(');
-});
+app.get('/auth/google/callback',
+    passport.authenticate('google', {
+        successRedirect : '/logged',
+        failureRedirect: '/'
+    }));
+
+app.listen(3000);
